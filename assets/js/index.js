567faecc98e2f4973c30f70373190316b52a49ff
wc-su <theblissout@gmail.com>
2023-08-21 17:25:50 +0800

    (1)修正平板時畫面出現x軸的問題。(2)調整平板手機的滾動特效

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,54 +7,83 @@ gsap.registerPlugin(ScrollTrigger);
 //   order: 順向進入(onEnter)、順向離開(onLeave)、逆向進入(onEnterBack)、逆向離開(onLeaveBack)
 //   value: play, pause, resume, reset, restart, complete, reverse, none
 const toggleActions = {
-  toggleActions: "play pause resume reset"
+  toggleActions: "play pause resume reset",
 };
 
-// 推銷
-const promoteTimeline = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".promote h3",
-    // markers: true,
-    // start: "top 80%",
-    // end: "bottom 80%",
-    ...toggleActions,
+// Responsive media
+// https://greensock.com/docs/v3/GSAP/gsap.matchMedia()
+let mm = gsap.matchMedia(), breakPoint = 992;
+mm.add(
+  {
+    isDesktop: `(min-width: ${breakPoint}px)`,
+    isTablet: `(max-width: ${breakPoint - 0.02}px)`,
+    isMobile: `(max-width: ${768 - 0.02}px)`,
   },
-});
-promoteTimeline
-  .from(".promote h3", { y: 100, opacity: 0, duration: 0.6 })
-  .from(".promote-item", { y: "random(80, 120)", opacity: 0, duration: 0.8 });
+  (context) => {
+    let { isDesktop, isTablet, isMobile } = context.conditions;
 
-// 經典系列鏡框
-const baicTimeline = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".basic h3",
-    ...toggleActions,
-  },
-});
-baicTimeline
-  .from(".basic h3", { y: 100, opacity: 0, duration: 0.6 })
-  .from(".basic-item", { y: "random(80, 120)", opacity: 0, duration: 0.8 });
+    // 推銷
+    const promoteTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".promote",
+        // markers: true,
+        // start: "top 80%",
+        // end: "bottom 80%",
+        ...toggleActions,
+      },
+    });
+    if (isDesktop) {
+      promoteTimeline
+        .from(".promote h3", { y: 100, opacity: 0, duration: 0.6 })
+        .from(".promote-item", {y: "random(80, 120)", opacity: 0, duration: 0.8,});
+    } else {
+      promoteTimeline
+        .from(".promote h3", { y: 100, opacity: 0, duration: 0.6 })
+        .from(".promote-item:nth-child(odd)", {y: "random(80, 120)", opacity: 0, duration: 0.8, stagger: 0.8,})
+        .from(".promote-item:nth-child(even)", { y: "random(80, 120)", opacity: 0, duration: 0.8, stagger: 0.8 }, "-=1.6");
+    }
 
-// 聯名設計鏡框
-const brandingTimeline = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".co-branding h3",
-    ...toggleActions,
-  },
-});
-brandingTimeline
-  .from(".co-branding h3", { y: 100, opacity: 0, duration: 0.6 })
-  .from(".co-branding-item:nth-child(1)", { x: 50, y: 50, opacity: 0, scale: 1.2, duration: 0.6 })
-  .from(".co-branding-item:nth-child(2)", { x: -50, y: -50, opacity: 0, scale: 1.2, duration: 0.6 }, "-=0.6");
+    // 經典系列鏡框
+    const baicTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".basic",
+        ...toggleActions,
+      },
+    });
+    baicTimeline
+      .from(".basic h3", { y: 100, opacity: 0, duration: 0.6 })
+      .from(".basic-item", { y: "random(80, 120)", opacity: 0, duration: 0.8, stagger: isDesktop ? 0 : 0.6,});
 
-// 顧客推薦
-const recommendTimeline = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".recommend h3",
-    ...toggleActions,
-  },
-});
-recommendTimeline
-  .from(".recommend h3", { y: 100, opacity: 0, duration: 0.6 })
-  .from(".recommend-item:nth-child(odd)", { y: 80, opacity: 0, duration: 1.2 })
-  .from(".recommend-item:nth-child(even)",{ y: -50, opacity: 0, duration: 1.2 },"-=1.2");
+    // 聯名設計鏡框
+    const brandingTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".co-branding",
+        ...toggleActions,
+      },
+    });
+    brandingTimeline
+      .from(".co-branding h3", { y: 100, opacity: 0, duration: 0.6 })
+      .from(".co-branding-item:nth-child(1)", { x: isDesktop ? 50 : 0, y: 50, opacity: 0, duration: 0.6, })
+      .from(".co-branding-item:nth-child(2)", { x: isDesktop ? -50 : 0, y: -50, opacity: 0, duration: 0.6, }, isDesktop ? "-=0.6" : null);
+
+    // 顧客推薦
+    const recommendTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".recommend",
+        ...toggleActions,
+      },
+    });
+    if(isMobile) {
+      recommendTimeline
+      .from(".recommend h3", { y: 100, opacity: 0, duration: 0.6 })
+      .from(".recommend-item", { y: 80, opacity: 0, duration: 0.8, stagger: isDesktop ? 0 : 0.8 });
+    } else {
+      recommendTimeline
+        .from(".recommend h3", { y: 100, opacity: 0, duration: 0.6 })
+        .from(".recommend-item:nth-child(odd)", { y: 80, opacity: 0, duration: 0.8, stagger: isDesktop ? 0 : 0.8 })
+        .from(".recommend-item:nth-child(even)", { y: -50, opacity: 0, duration: 0.8, stagger: isDesktop ? 0 : 0.8 }, isDesktop ? "-=0.8" : "-=1.6");
+    }
+
+    // return () => {}
+  }
+);
